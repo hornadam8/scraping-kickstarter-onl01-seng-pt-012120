1710cbe2d2ffb8fe57298cc0a5a8c541d23d162e
Adam G Horn <hornadam8@gmail.com>
2020-02-23 21:31:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,7 +1,11 @@
-require Nokogiri
+require "nokogiri"
+require "pry"
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
+  binding.pry
 end
+
+create_project_hash
 end
