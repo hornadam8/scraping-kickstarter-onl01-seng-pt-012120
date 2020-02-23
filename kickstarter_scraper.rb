37ffb94c99e196afef83d0ef952cfec146a61d75
Adam G Horn <hornadam8@gmail.com>
2020-02-23 21:43:33 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
+  
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
