0db146d0e87ebb509d4de0068676420d25e6e5c5
Adam G Horn <hornadam8@gmail.com>
2020-02-23 21:40:56 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -4,11 +4,11 @@ require "pry"
 def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
-    
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
   end
- 
   projects
 end
 
-create_project_hash
+
 # projects: kickstarter.css("li.project.grid_4")
